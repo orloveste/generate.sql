12a53bef25e30e703b88fbdb85006b4304b2d518
orloveste <orlovevm@yahoo.com>
2020-01-12 12:53:01 +0200

    update_alter_view_insertSelectWith_transaction part1 12.01.2020

--- src/main/resources/generate.sql
+++ src/main/resources/generate.sql
@@ -1,47 +1,23 @@
 create schema scoala;
 
-CREATE TABLE scoala.clasa(
-    idClasa int(5) unsigned auto_increment primary key ,
+create table scoala.clasa
+(
+    idClasa   int(5) unsigned auto_increment primary key,
     numeClasa varchar(10) not null
 );
-CREATE TABLE scoala.elevi(
-    idElev int(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+
+create table scoala.elevi
+(
+    idElev   int(5) unsigned auto_increment primary key,
     numeElev varchar(25) not null,
-    idClasa int(5) unsigned,
+    idClasa  int(5) unsigned,
     foreign key (idClasa) references scoala.clasa(idClasa)
 );
-Create table scoala.note(
-    idNote int(5) unique auto_increment primary key ,
-    valoareNota int(2)not null ,
-    idElev int(5) unsigned
-);
-insert into scoala.note(valoareNota, idElev) VALUES (3,2);
-insert into scoala.note(valoareNota, idElev) VALUES (5,11);
-insert into scoala.note(valoareNota, idElev) VALUES (10,1);
-insert into scoala.note(valoareNota, idElev) VALUES (7,9);
-insert into scoala.note(valoareNota, idElev) VALUES (8,7);
-insert into scoala.note(valoareNota, idElev) VALUES (6,1);
-insert into scoala.note(valoareNota, idElev) VALUES (9,1);
-insert into scoala.note(valoareNota, idElev) VALUES (3,3);
-insert into scoala.note(valoareNota, idElev) VALUES (7,10);
-insert into scoala.note(valoareNota, idElev) VALUES (6,11);
-insert into scoala.note(valoareNota, idElev) VALUES (9,8);
-insert into scoala.note(valoareNota, idElev) VALUES (10,1);
-insert into scoala.note(valoareNota, idElev) VALUES (6,2);
-insert into scoala.note(valoareNota, idElev) VALUES (7,8);
-insert into scoala.note(valoareNota, idElev) VALUES (8,7);
-insert into scoala.note(valoareNota, idElev) VALUES (7,6);
-insert into scoala.note(valoareNota, idElev) VALUES (9,4);
-insert into scoala.note(valoareNota, idElev) VALUES (10,4);
-insert into scoala.note(valoareNota, idElev) VALUES (7,10);
-insert into scoala.note(valoareNota, idElev) VALUES (8,11);
-insert into scoala.note(valoareNota, idElev) VALUES (9,5);
 
 insert into scoala.clasa(numeClasa) values ('10A');
 insert into scoala.clasa(numeClasa) values ('10B');
 insert into scoala.clasa(numeClasa) values ('10C');
 
-insert into scoala.elevi(numeElev, idClasa) VALUES ('gigel', 1);
 insert into scoala.elevi(numeElev, idClasa) VALUES ('Fratili Mic', 1);
 insert into scoala.elevi(numeElev, idClasa) VALUES ('Romu Vasile', 2);
 insert into scoala.elevi(numeElev, idClasa) VALUES ('Gamu Ramon', 2);
@@ -53,49 +29,38 @@ insert into scoala.elevi(numeElev, idClasa) VALUES ('Romulus Cane', 2);
 insert into scoala.elevi(numeElev, idClasa) VALUES ('Zorel Cana', 1);
 insert into scoala.elevi(numeElev, idClasa) VALUES ('Zara Buni', 3);
 
-select*from scoala.clasa;
-select numeClasa from scoala.clasa;
-select idClasa, numeClasa from scoala.clasa;
-select idClasa, numeClasa from scoala.clasa where numeClasa like '10%';
-select idClasa, numeClasa from scoala.clasa where numeClasa like '%b';
-select idClasa, numeClasa from scoala.clasa where idClasa=2;
-select idClasa, numeClasa from scoala.clasa where numeClasa= '10a';
-select idClasa, numeClasa from scoala.clasa where idClasa > 1 and idClasa<4;
-select * from scoala.clasa join scoala.elevi on clasa.idClasa = elevi.idClasa;
-select * from scoala.clasa join scoala.elevi on clasa.idClasa = elevi.idClasa
-where numeClasa = '10b'
-  and numeElev like 'ro%';
+CREATE TABLE scoala.note
+(
+    idNota      INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    valoareNota INT(2) UNSIGNED,
+    idElev      INT(5) UNSIGNED ,
+    FOREIGN KEY (idElev) REFERENCES scoala.elevi (idElev)
+);
 
-select idElev, numeElev from scoala.clasa join scoala.elevi on clasa.idClasa = elevi.idClasa where numeClasa = '10b';
-#2
-select numeElev, idElev from scoala.clasa join scoala.elevi on clasa.idClasa = elevi.idClasa where numeClasa = '10b' and idElev>6;
-#3
-select numeElev, idElev from scoala.clasa join scoala.elevi on clasa.idClasa = elevi.idClasa where numeClasa='10c' and numeElev like '%a';
-#4
-select idElev from scoala.clasa join scoala.elevi on clasa.idClasa = elevi.idClasa
-where numeElev like 'a%' and numeElev like '%a';
-#acelas lucru
-select  idElev from scoala.elevi
-where numeElev like 'a%' and numeElev like '%a';
-#5
-select count(*) from scoala.elevi join scoala.clasa
-on clasa.idClasa=elevi.idClasa where numeClasa='10b';
+INSERT INTO scoala.note(valoareNota, idElev)
+VALUES (9, 1), (5, 1), (4, 2), (10, 2), (8, 3), (1, 3), (4, 4), (8, 4), (10, 5), (2, 5), (7, 6), (3, 6), (8, 7),
+       (1, 7), (10, 8),(10, 8), (2, 9), (10, 9), (5, 10), (5, 10);
 
-select count(*) from scoala.elevi join scoala.clasa
-on clasa.idClasa=elevi.idClasa where numeClasa='10b'
-and clasa.idClasa >100;
+update scoala.elevi set numeElev = 'Baronu Vasile' where idElev =2;
 
-select idClasa from scoala.clasa;
+alter table scoala.elevi add column varsta int(2);
 
-select sum(idClasa) from scoala.clasa;
-select min(idClasa) from scoala.clasa;
-select max(idClasa) from scoala.clasa;
-select avg(idClasa) from scoala.clasa;
-select count(idClasa) from scoala.clasa;
-select count(idElev) from scoala.elevi;
+alter table scoala.elevi change column varsta varstraElev int(2);
 
-select *from scoala.elevi;
-select *from scoala.elevi order by numeElev;
-select *from scoala.elevi order by numeElev, idClasa;#nu se sorteaza dupa doua coloane simultan
-select *from scoala.elevi order by idClasa desc;
-select *from scoala.elevi limit 4 offset 2;
+alter table scoala.elevi drop column  varstraElev;
+
+select numeElev, clasa.numeClasa  from scoala.clasa join scoala.elevi
+                                                  on clasa.idClasa = elevi.idClasa where numeClasa = '10b' and numeElev like '%l';
+#view de gasit
+create view eleviDin10BCuLiteraLafinal as
+select numeElev, clasa.numeClasa  from scoala.clasa join scoala.elevi
+                                                  on clasa.idClasa = elevi.idClasa where numeClasa = '10b' and numeElev like '%l';
+
+start transaction ;#cu si fara
+insert into scoala.note(valoareNota, idElev) VALUES (5,1);
+insert into scoala.note(valoareNota, idElev) VALUES (6,1);
+insert into scoala.note(valoareNota, idElev) VALUES (7,1);
+insert into scoala.note(valoareNota, idElev) VALUES (8,1);
+insert into scoala.note(valoareNota, idElev) VALUES (9,1);
+insert into scoala.note(valoareNota, idElev) VALUES (10,1);
+commit;#cu si fara
